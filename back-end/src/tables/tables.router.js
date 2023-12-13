1be99770bec09c6b6db8cd4b40630cac1589d3d7
Clarisse <clarisse421@berkeley.edu>
2023-12-13 15:13:50 -0500

    Added methods not allowed to table and reservations router

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -5,13 +5,19 @@
  */
 
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./tables.controller");
 
-router.route("/").get(controller.list).post(controller.create);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 router
   .route("/:table_id/seat")
   .put(controller.update)
   .get(controller.read)
-  .delete(controller.destroy);
+  .delete(controller.destroy)
+  .all(methodNotAllowed);
 
 module.exports = router;
